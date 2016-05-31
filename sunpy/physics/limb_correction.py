02b39a8c4680a2b7898e517c4bfc092c08abbe29
CyclingNinja <bennett.sm89@gmail.com>
2016-05-31 21:52:14 +0000

    First passing test checks that the map that is passed in remains the same on the way out

--- sunpy/physics/limb_correction.py
+++ sunpy/physics/limb_correction.py
@@ -3,18 +3,22 @@ import pytest
 import numpy as np
 import astropy.units as u
 import sunpy.map
-
+import sunpy.data.test
+import os
 
 
 
 @pytest.fixture
 def hsi_image():
-    testpath = sunpy.data.test.rootdit
+    testpath = sunpy.data.test.rootdir
     return sunpy.map.Map(os.path.join(testpath, 'hsi_image_20101016_191218.fits'))
 
+
+
 def test_limb_correct():
-    assert isinstace(limb_correct(hsi_image), sunpy.map)
+    assert isinstance(limb_correct(hsi_image()), type(hsi_image()))
 
 
 
-def limb_correct():
+def limb_correct(amap):
+    return amap
